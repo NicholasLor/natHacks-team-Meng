ebc0501bf4d8db2eeea1e5284eada0f0636cd67a
kWeech <kenneth.weech@gmail.com>
2022-07-30 18:32:45 -0600

    added final score screen and player 2

--- MindGame/game.py
+++ MindGame/game.py
@@ -22,12 +22,12 @@ def obstacle_movement(obstacle_list):
         return []
 
 
-def collision(player, obstacles):
+def collision(player1, player2, obstacles):
     if obstacles:
         for obstacle_rect in obstacles:
-            if player.colliderect(obstacle_rect):
-                return 2
-    return 1
+            if player1.colliderect(obstacle_rect) or player2.colliderect(obstacle_rect):
+                return True
+    return False
 
 pygame.init()
 
@@ -55,13 +55,13 @@ background = pygame.image.load('MindGame/graphics/background.png').convert_alpha
 
 # player1
 player1 = pygame.image.load('MindGame/graphics/brain_sprite_v1.png').convert_alpha()
-player1_rect = player1.get_rect(midbottom = (150,ground_height))
+player1_rect = player1.get_rect(midbottom = (250,ground_height))
 player1_grav = 0
 jump1 = False
 
 # player2
 player2 = pygame.image.load('MindGame/graphics/brain_sprite_v1.png').convert_alpha()
-player2_rect = player1.get_rect(midbottom = (200,ground_height))
+player2_rect = player2.get_rect(midbottom = (120,ground_height))
 player2_grav = 0
 jump2 = False
 
@@ -75,7 +75,7 @@ obstacle_rect_list = []
 
 # obstacle timer
 obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer, 1200)
+pygame.time.set_timer(obstacle_timer, 1500)
 
 
 # game loop
@@ -86,7 +86,10 @@ while True:
             sys.exit()
 
         if event.type == obstacle_timer and game_state == 1:
-            obstacle_rect_list.append(enemy.get_rect(midbottom = (randint(screen_width,screen_width*1.3),ground_height)))
+            if randint(0,2):    
+                obstacle_rect_list.append(enemy.get_rect(midbottom = (randint(screen_width,screen_width*1.35),ground_height)))
+            else:
+                obstacle_rect_list.append(enemy.get_rect(midbottom = (randint(screen_width,screen_width*1.35),ground_height-150)))
         
         # #TODO handling for eeg input
         # #will have to bring it out of the event loop
@@ -115,6 +118,9 @@ while True:
 
         if jump1 == True:
             player1_grav = -25
+        
+        if jump2 == True:
+            player2_grav = -25
 
         player1_grav += 1
         player1_rect.y += player1_grav
@@ -122,29 +128,50 @@ while True:
             player1_rect.bottom = ground_height
         screen.blit(player1,player1_rect)
 
-        # screen.blit(enemy,enemy_rect)
-        # enemy_rect.left -= enemy_vel
-        # if(enemy_rect.left <= -100):
-        #     enemy_rect.left = screen_width + 100
+        player2_grav += 1
+        player2_rect.y += player2_grav
+        if player2_rect.bottom >= ground_height:
+            player2_rect.bottom = ground_height
+        screen.blit(player2,player2_rect)
 
         # obstacle movement
         obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
-        game_state = collision(player1_rect, obstacle_rect_list)
-
-        # if(player1_rect.colliderect(enemy_rect)):
-        #     game_state = 2
+        if(collision(player1_rect, player2_rect, obstacle_rect_list)):
+            game_state = 2
+            final_score = int((pygame.time.get_ticks() - start_time)/100)
+            restart_timer = pygame.time.get_ticks() + 5000
 
     # end game screen
     elif(game_state == 2):
-        screen.fill('white')
+        screen.blit(background,(0,0))
+
+        #resetting enemies and players
         obstacle_rect_list = []
-        # enemy_rect.left = screen_width + 100
-        if(jump1 == True):
-            game_state = 1
-            start_time = pygame.time.get_ticks()
+        player1_rect.bottom = ground_height
+        player2_rect.bottom = ground_height
+
+        #displaying final score
+        final_surf = text_font.render("FINAL SCORE: " + f'{final_score}', False,(0,0,0))
+        final_rect = final_surf.get_rect(center = (screen_width/2,screen_height/2-100))
+        screen.blit(final_surf,final_rect)
+
+        #timer to restart
+        
+
+        if(restart_timer > pygame.time.get_ticks()):
+            replay_surf = text_font.render("ABLE TO REPLAY IN: " + f'{int((restart_timer - pygame.time.get_ticks())/1000)}', False,(0,0,0))
+        else:
+            replay_surf = text_font.render("JUMP TO REPLAY", False,(0,0,0))
+            # jump to restart
+            if(jump1 == True or jump2 == True):
+                game_state = 1
+                start_time = pygame.time.get_ticks()
+        replay_rect = final_surf.get_rect(center = (screen_width/2,screen_height/2))
+        screen.blit(replay_surf,replay_rect)
 
     
     pygame.display.update()
     clock.tick(60)
     jump1 = False
+    jump2 = False
